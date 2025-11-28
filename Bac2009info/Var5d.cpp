3efdeab97af7b6e6258c350286802a5d59321042
KraK7 <KraK7@users.noreply.github.com>
2025-11-28 10:15:54 +0200

    Added full CTF project structure and files

---
+++ Bac2009info/Var5d.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,z,y;
+	cin >> x >> z;
+	y = 0;
+	do
+	{
+		y=y*10+x%10;
+		x=x/100;
+	}while(x!=0);
+
+	while(y*z>0 && y%10==z%10)
+	{
+		y=y/10;
+		z=z/10;
+	}
+	if(y+z==0)
+	{
+		cout << "1";
+	}
+	else
+	{
+		cout << "0";
+	}
+        return 0;
+
+}
