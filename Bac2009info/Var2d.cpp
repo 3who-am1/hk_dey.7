568342b21cc035f951c2b91f4780c6e073185407
KraK7 <KraK7@users.noreply.github.com>
2025-11-28 09:39:03 +0200

    Added full CTF project structure and files

---
+++ Bac2009info/Var2d.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x;
+	while(x>0)
+	{
+		cin >> y;
+		if(x>y)
+		{
+			cout << x%10;
+		}
+		else
+		{
+			cout << y%10;
+		}
+		x=y;
+	}
+	return 0;
+}
